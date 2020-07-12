f28514adb45f7d6612ff9707982b80528400272e
Edward Chen <edwardchen805@gmail.com>
2020-07-12 16:44:19 -0700

    Refactor on solution review
    - Also added Part 2: adding a timer flag

--- main.go
+++ main.go
@@ -1,45 +1,73 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
-	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	quiz := make(map[string]int)
-	pwd, _ := os.Getwd()
-	f, _ := os.Open(filepath.Join(pwd,"problems.csv"))
-	r := csv.NewReader(bufio.NewReader(f))
-	fmt.Println("Preparing the quiz, get ready!")
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		i, err := strconv.Atoi(record[1])
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(2)
-		}
-		quiz[record[0]] = i
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		// TODO: Why not Printf?
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
 	}
-	fmt.Println("It's quiz time! Input your answer and press Enter when you are ready. " +
-		"Note: Once you press Enter, you cannot go back to a previous question.")
-	reader := bufio.NewReader(os.Stdin)
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse CSV file.")
+	}
+	problems := parseLines(lines)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	correct := 0
-	for prob, ans := range quiz {
-		fmt.Println(prob)
-		text, _,_ := reader.ReadLine()
-		userInput, _ := strconv.Atoi(strings.TrimSpace(string(text)))
-		if ans == userInput {
-			correct++
+problemLoop:
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer) // Scanf trims input
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C: // if the value from Timer is sent on channel C, do the following conditions
+			fmt.Println()
+			break problemLoop // Golang's equivalent of goto
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
-	fmt.Printf("There were %d questions and you got %d correct.", len(quiz), correct)
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines)) //Create an array of problem struct
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1) // exit code 1 == the application experienced an error of some sort
 }
